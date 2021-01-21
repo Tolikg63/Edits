b6f1230683f56fa32ec18a98d885d6a4ce9d10c9
Tolikg63 <ttserunyan@inbox.ru>
2021-01-21 13:31:21 -0800

    All in

---
+++ index.js
@@ -0,0 +1,25 @@
+{
+    // "workbench.statusBar.visible": false,
+    "files.autoSave": "afterDelay",
+    "window.zoomLevel": 0,
+    "workbench.startupEditor": "newUntitledFile",
+    "workbench.activityBar.visible": false,
+    "editor.fontSize": 19,
+    "editor.fontWeight": "400",
+    "editor.tabSize": 3,
+    "editor.glyphMargin": false,
+    "editor.smoothScrolling": true,
+    "workbench.iconTheme": "material-icon-theme",
+    "workbench.colorTheme": "Material Theme Darker",
+    "editor.fontFamily": "Fira Code, Consolas",
+    "editor.wordWrap": "on",
+    "terminal.external.windowsExec": "C:\\Program Files\\Git\\bin\\bash.exe",
+    "terminal.integrated.shell.windows": "C:\\Program Files\\Git\\bin\\bash.exe"
+    "liveSassCompile.settings.formats": [
+        {
+            "format": "expanded",
+            "extensionName": ".css",
+            "savePath": "/css"
+        }
+    ]
+}
